d232c6ce13732dc13b20de4991653b77fc0ab82e
mitanshu-2004 <mitanshug2004@gmail.com>
2024-05-14 12:16:32 +0530

    changed bot controller

--- hexapod_control/scripts/bot_controller.py
+++ hexapod_control/scripts/bot_controller.py
@@ -8,26 +8,23 @@ class Mynode(Node):
     def __init__(self):
         super().__init__("rviz_to_gazebo")
         self.get_logger().info("Rviz to Gazebo started")
-        self.cmd_vel_pub_=self.create_publisher(Float64MultiArray,"/hexapod_controller/commands",10)
-        self.timer=self.create_timer(0.5,self.send_value)
-    
+        self.cmd_vel_pub_ = self.create_publisher(Float64MultiArray, "/hexapod_controller/commands", 10)
+        self.timer = self.create_timer(0.5, self.send_value)
+        self.msg = Float64MultiArray()
+        self.msg.data = [0.0] * 18
+
     def send_value(self):
-        msg = Float64MultiArray()
-        self.joint_input=int(input("Joint to move: "))
-        self.joint_value=float(input("Value for the joint: "))
-        msg.data=[0.0] * 18 
-        msg.data[self.joint_input-1]=self.joint_value 
-        self.cmd_vel_pub_.publish(msg)
-        self.get_logger().info('Publishing: %s' % msg.data)
+        self.joint_input = int(input("Joint to move: "))
+        self.joint_value = float(input("Value for the joint: "))
+        self.msg.data[self.joint_input - 1] = self.joint_value
+        self.cmd_vel_pub_.publish(self.msg)
+        self.get_logger().info('Publishing: %s' % self.msg.data)
 
 def main(args=None):
     rclpy.init(args=args)
-
-    node=Mynode()
-    
+    node = Mynode()
     rclpy.spin(node)
-
     rclpy.shutdown()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
