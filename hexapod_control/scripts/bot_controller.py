0abef506092c2ecc71735f933ca9dfa605cc5835
mitanshu-2004 <mitanshug2004@gmail.com>
2024-05-22 22:35:31 +0530

    Added position and velocity inputs for joints

--- hexapod_control/scripts/bot_controller.py
+++ hexapod_control/scripts/bot_controller.py
@@ -1,27 +1,72 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64MultiArray
+import sys
 
 class Mynode(Node):
-    def __init__(self):
-        super().__init__("bot_control")
-        self.cmd_vel_pub_ = self.create_publisher(Float64MultiArray, "/hexapod_controller/commands", 10)
-        self.timer = self.create_timer(0.5, self.send_value)
-        self.msg = Float64MultiArray()
-        self.msg.data = [0.0] * 18
+    def __init__(self, target_positions, velocities):
+        super().__init__("position_subscriber")
+        self.get_logger().info("Position Subscriber started")
+        
+        self.joint_array = [
+            'Revolute_63', 'Revolute_64', 'Revolute_65', 'Revolute_69', 
+            'Revolute_70', 'Revolute_71', 'Revolute_72', 'Revolute_73', 
+            'Revolute_74', 'Revolute_75', 'Revolute_76', 'Revolute_77', 
+            'Revolute_78', 'Revolute_79', 'Revolute_80', 'Revolute_81', 
+            'Revolute_82', 'Revolute_83'
+        ]
 
-    def send_value(self):
-        self.joint_input = int(input("Joint to move: "))
-        self.joint_value = float(input("Value for the joint: "))
-        self.msg.data[self.joint_input - 1] = self.joint_value
-        self.cmd_vel_pub_.publish(self.msg)
-        self.get_logger().info('Publishing: %s' % self.msg.data)
+        self.target_positions = target_positions  
+        self.velocities = velocities 
+        
+        self.pose_subscriber = self.create_subscription(
+            JointState,
+            "/joint_states",
+            self.joint_callback,
+            10
+        )
+
+        self.cmd_vel_pub_ = self.create_publisher(Float64MultiArray, "/hexapod_controller/commands", 10)
+
+    def joint_callback(self, msg: JointState):
+        self.send_velocities = Float64MultiArray()
+        self.send_velocities.data = [0.0] * 18
+
+        for i in range(len(msg.position)):
+            joint_name = msg.name[i]
+            if joint_name in self.joint_array:
+                joint_index = self.joint_array.index(joint_name)
+                current_position = msg.position[i]
+                target_position = self.target_positions[joint_index]
+                velocity = self.velocities[joint_index]
+
+                if current_position < target_position:
+                    self.send_velocities.data[joint_index] = velocity  
+                elif current_position > target_position:
+                    self.send_velocities.data[joint_index] = -velocity  
+                else:
+                    self.send_velocities.data[joint_index] = 0.0 
+
+        self.get_logger().info('Publishing: %s' % self.send_velocities.data)
+        self.cmd_vel_pub_.publish(self.send_velocities)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Mynode()
+
+    if args is None:
+        args = sys.argv
+
+    target_positions = [0.0] * 18  
+    velocities = [0.1] * 18  
+
+    
+    if len(args) > 1:
+        target_positions = list(map(float, args[1:19]))
+        velocities = list(map(float, args[19:37]))
+
+    node = Mynode(target_positions, velocities)
     rclpy.spin(node)
     rclpy.shutdown()
 
