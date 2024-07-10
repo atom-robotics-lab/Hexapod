7c26e4386fef27b0b87e37d3918b58f331aa22c1
mitanshu-2004 <mitanshug2004@gmail.com>
2024-07-10 13:57:48 +0530

    joint position control

--- hexapod_control/scripts/bot_controller.py
+++ hexapod_control/scripts/bot_controller.py
@@ -3,21 +3,87 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
+from time import sleep
 
 class Mynode(Node):
     def __init__(self):
         super().__init__("bot_control")
         self.cmd_vel_pub_ = self.create_publisher(Float64MultiArray, "/hexapod_controller/commands", 10)
-        self.timer = self.create_timer(0.5, self.send_value)
         self.msg = Float64MultiArray()
         self.msg.data = [0.0] * 18
-
-    def send_value(self):
-        self.joint_input = int(input("Joint to move: "))
-        self.joint_value = float(input("Value for the joint: "))
-        self.msg.data[self.joint_input - 1] = self.joint_value
         self.cmd_vel_pub_.publish(self.msg)
         self.get_logger().info('Publishing: %s' % self.msg.data)
+        sleep(2)
+
+        self.msg.data[7]=0.2
+        self.msg.data[9]=-0.2
+        self.msg.data[11]=-0.2
+        self.cmd_vel_pub_.publish(self.msg)
+        self.get_logger().info('1')
+        self.get_logger().info('Publishing: %s' % self.msg.data)
+        sleep(2)
+        while True:
+
+            self.msg.data[1]=0.2
+            self.msg.data[3]=-0.2
+            self.msg.data[5]=-0.2
+            self.cmd_vel_pub_.publish(self.msg)
+            
+            self.get_logger().info('Publishing: %s' % self.msg.data)
+            sleep(2)
+
+            self.msg.data[7]=0.0
+            self.msg.data[9]=0.0
+            self.msg.data[11]=0.0
+            self.cmd_vel_pub_.publish(self.msg)
+            self.get_logger().info('Publishing: %s' % self.msg.data)
+            sleep(2)
+
+            self.msg.data[6]=0.2
+            self.msg.data[8]=0.2
+            self.msg.data[10]=-0.2
+            self.cmd_vel_pub_.publish(self.msg)
+            
+            self.get_logger().info('Publishing: %s' % self.msg.data)
+            sleep(2)
+
+            self.msg.data[1]=-0.2
+            self.msg.data[3]=0.2
+            self.msg.data[5]=0.2
+            self.msg.data[0]=0.2
+            self.msg.data[2]=0.2
+            self.msg.data[4]=-0.2
+            self.cmd_vel_pub_.publish(self.msg)
+            
+            self.get_logger().info('Publishing: %s' % self.msg.data)
+            sleep(2)
+
+            self.cmd_vel_pub_.publish(self.msg)
+           
+            self.msg.data[6]=0.0
+            self.msg.data[8]=0.0
+            self.msg.data[10]=0.0
+            self.msg.data[7]=0.2
+            self.msg.data[9]=-0.2
+            self.msg.data[11]=-0.2
+            self.cmd_vel_pub_.publish(self.msg)
+            
+            self.get_logger().info('Publishing: %s' % self.msg.data)
+            sleep(2)
+            
+            self.msg.data[0]=-0.2
+            self.msg.data[2]=-0.2
+            self.msg.data[4]=0.2
+
+
+
+        
+
+
+        
+
+
+        
 
 def main(args=None):
     rclpy.init(args=args)
